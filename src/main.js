e095ad0515dfbd4cbf3b699f5ec097d9c34b8c0c
Spencer <spenrue@gmail.com>
2020-10-02 17:55:12 -0500

    Initial commit of guitar app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import bulma from 'bulma';
+import '..\\node_modules\\bulma-tooltip\\dist\\css\\bulma-tooltip.min.css';
+import 'bulma-tooltip';
 
 Vue.config.productionTip = false
+
+Vue.use(bulma);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
